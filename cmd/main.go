258307f91f8983861cd3485a585cfb0422dbba36
Marcelo Junior <marcelo.jr63@gmail.com>
2024-12-01 17:54:13 -0300

    Create a script to test Peer#Handshake

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/juneira/torrent-study/torrent"
 )
 
 func main() {
 	t, _ := torrent.FromFilename("tests/fixtures/debian-12.8.0-amd64-netinst.iso.torrent")
 
-	fmt.Println(t.Announce)
-
 	pid := [20]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11}
-	t.GetPeers(pid, 80)
+	peers, err := t.GetPeers(pid, 80)
+	if err != nil {
+		panic(err)
+	}
+
+	peer := peers[0]
+	peerConn, err := torrent.NewPeerConn(&peer)
+	if err != nil {
+		panic(err)
+	}
+
+	peer.SetConnection(peerConn)
+
+	peer.Handshake(t.InfoHash, pid)
 }
